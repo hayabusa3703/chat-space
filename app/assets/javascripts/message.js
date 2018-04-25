a3db3644b59fd89dd803a46f90555f9e2c373f25
hayabusa3703 <umibudou3703@gmail.com>
2018-04-25 19:49:30 +0900

    asynchronous_system_create

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,7 +1,53 @@
+$(document).on('turbolinks:load', function(){
 $(function(){
+  function buildHTML(message) {
+    //${ message.image.url }
+    if (message.image) {
+      var image = `<img src=${message.image}>`;
+      console.log(image);
+    } else {
+      var image = "";
+    }
+   var html = `<div class="chat__main__body__message--name">
+                 ${message.user_name}
+               </div>
+               <div class="chat__main__body__message--time">
+                 ${message.time}
+               </div>
+               <div class="chat__main__body__message--text">
+                 <p class="lower-message__content">
+                 ${message.content}
+                 </p>
+               ${image}
+               </div>`
+      return html;
+  }
+
   $('#new_message').on('submit', function(e){
     e.preventDefault();
-    console.log(this)
     var formData = new FormData(this);
+    var url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      console.log(data.image);
+      var html = buildHTML(data);
+      $('.chat__main__body__message').append(html);
+      $('#message_image').val('');
+      $('.form__message').val('');
+      $('.chat__main__body').animate({scrollTop: $('.chat__main__body')[0].scrollHeight}, 'swing');
+      $('.form__submit').prop("disabled", false)
+    })
+    .fail(function(){
+      alert('error');
+    })
   })
-})
+});
+return false
+});
